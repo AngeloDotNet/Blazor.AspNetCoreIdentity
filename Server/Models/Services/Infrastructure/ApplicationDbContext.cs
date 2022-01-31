51be871f58a9e382b3e68b88f619549ddd9c536f
Angelo Pirola <angelo9871@gmail.com>
2022-01-31 13:07:32 +0100

    Aggiunto ASP.NET Core Identity

--- Server/Models/Services/Infrastructure/ApplicationDbContext.cs
+++ Server/Models/Services/Infrastructure/ApplicationDbContext.cs
@@ -11,5 +11,10 @@ namespace DemoBlazorAuthentication.Server.Models.Services.Infrastructure
         public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
